e8524b453ab881c57804c3f6c32a8f1e60f37872
Franciszek Pindel <franciszek.pin@gmail.com>
2020-10-30 11:23:56 +0100

    Implement executing command from main loop

--- main.c
+++ main.c
@@ -1,12 +1,20 @@
 #include<stdio.h>
+#include <memory.h>
 #include "constants.h"
 #include "input.h"
 #include "command_interpretation.h"
 #include "text_manipulation.h"
+#include "command_execution.h"
 
 int main() {
     static char input_tape[N];
     static char command_tape[N];
+    static char result_tape[N];
+    static char clipboard[N];
+
+    clipboard[0] = '\0';
+
+    strcpy(result_tape, command_tape);
 
     readInputTape(input_tape);
     readCommandTape(command_tape);
@@ -19,11 +27,14 @@ int main() {
         if (command_tape[cursor] != '\0') {
             cursor++;
             struct Command command = interpretCommand(getCommand(cursor, command_tape));
-            printf("%d %d %d %s\n", command.commandType, command.firstArgument, command.secondArgumentNumber, command.secondArgumentText);
+            executeCommand(command, input_tape, result_tape, clipboard);
+            strcpy(input_tape, result_tape);
         }
 
 
     }
+
+    printf("%s\n", result_tape);
 
     return 0;
 }
